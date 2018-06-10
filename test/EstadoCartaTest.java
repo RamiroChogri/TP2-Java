6ab54aa04f8e2afa34c5b2bcd48ba76810bbf020
nicolas <nicolas@Nicolas-PC>
2018-06-10 20:57:13 -0300

    Test de estado en mazo

---
+++ test/EstadoCartaTest.java
@@ -0,0 +1,58 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public EstadoTest {
+	
+	//EstadoCartaEnMazo
+	
+	@Test
+	public void testEstadoCartaEnMazoEstaEnMazo() {
+		EstadoCarta estado = new EstadoCartaEnMazo();
+		assertEquals(true, estado.estaEnMazo());
+	}
+	
+	@Test
+	public void testEstadoCartaEnMazoNoEstaEnMano() {
+		EstadoCarta estado = new EstadoCartaEnMazo();
+		assertEquals(false, estado.estaEnMano());
+	}
+	
+	@Test
+	public void testEstadoCartaEnMazoNoEstaEnCementerio( ) {
+		EstadoCarta estado = new EstadoCartaEnMazo();
+		assertEquals(false, estado.estaEnCementerio());
+	}
+	
+	@Test
+	public void testEstadoCartaEnMazoNoEstaEnModoAtaque() {
+		EstadoCarta estado = new EstadoCartaEnMazo();
+		assertEquals(false, estado.estaEnModoAtaque());
+	}
+	
+	@Test
+	public void testEstadoCartaEnMazoNoEstaBocaArribaEnModoDefensa() {
+		EstadoCarta estado = new EstadoCartaEnMazo();
+		assertEquals(false, estado.estaBocaArribaEnModoDefensa());
+	}
+	
+	@Test
+	public void testEstadoCartaEnMazoNoEstaBocaAbajoEnModoDefensa() {
+		EstadoCarta estado = new EstadoCartaEnMazo();
+		assertEquals(false, estado.estaBocaAbajoEnModoDefensa());
+	}
+	
+	@Test
+	public void testEstadoCartaEnMazoNoEstaBocaArriba() {
+		EstadoCarta estado = new EstadoCartaEnMazo();
+		assertEquals(false, estado.estaBocaArriba());
+	}
+	
+	@Test
+	public void testEstadoCartaEnMazoNoEstaBocaAbajo() {
+		EstadoCarta estado = new EstadoCartaEnMazo();
+		assertEquals(false, estado.estaBocaAbajo());
+	}
+	
+	//Seguir con resto de estados
+	
+}
