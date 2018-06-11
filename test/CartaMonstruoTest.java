da615b2ee6ab97853a73af847a42c8245bf915c9
marcelo <marcelo@marcelo-PC-00>
2018-06-10 22:57:45 -0300

    public class para estado carta test y carta monstruo test

--- test/CartaMonstruoTest.java
+++ test/CartaMonstruoTest.java
@@ -1,7 +1,7 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public CampoTest {
+public class CampoTest {
 	
 	//La carta tiene 6 estados: En mazo, en mano, en campo ataque,
 	//en campo defensa boca arriba, en campo defensa boca abajo y en cementerio (muerta).
