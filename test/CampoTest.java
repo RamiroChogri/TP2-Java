ebeb1d2006647cf783b7a06d9cbefec68ad9bcd4
marcelo <marcelo@marcelo-PC-00>
2018-06-11 00:50:12 -0300

    AgegadosTest de CampoTest, y modificado Carta.java para que sea una interfaz

--- test/CampoTest.java
+++ test/CampoTest.java
@@ -6,4 +6,45 @@ public class CampoTest {
 	//La carta tiene 6 estados: En mazo, en mano, en campo ataque,
 	//en campo defensa boca arriba, en campo defensa boca abajo y en cementerio (muerta).
 	
+	@Test
+	public void testComienzaLaPartidaCon40CartasEnElMazo() {
+		Campo campoTest = new Campo();
+		assertEquals(40, campoTest.obtenerCantidadDeCartasEnMazo());
+	}
+	
+	@Test
+	public void testComienzaLaPartidaCon0CartasEnElCementerio() {
+		Campo campoTest = new Campo();
+		assertEquals(0, campoTest.obtenerCantidadDeCartasEnCementerio());
+	}
+	
+	@Test
+	public void testCampoComienzaVacio() {
+		Campo campoTest = new Campo();
+		assertEquals(false, campoTest.tieneCartas());
+	}
+	
+	@Test
+	public void testColocarUnMonstruoBocaArribaEnZonaCorrecta() {
+		Campo campoTest = new Campo();
+		Carta cartaTest = new CartaMonstruo();
+		campoTest.colocarCartaEnZonaMonstruo(cartaTest);
+		assertEquals(1, campoTest.obtenerCantidadDeCartasEnZonaMonstruo());
+	}
+	
+	@Test
+	public void testColocarUnaCartaMagicaEnZonaCorrecta() {
+		Campo campoTest = new Campo();
+		Carta cartaTest = new CartaMagica();
+		campoTest.colocarCartaEnZonaMagica(cartaTest);
+		assertEquals(1, campoTest.obtenerCantidadDeCartasEnZonaMagica());
+	}
+	
+	@Test
+	public void testColocarUnaCartaTrampaEnZonaCorrecte() {
+		Campo campoTest = new Campo();
+		Carta cartaTest = new Carta();
+		campoTest.colocarCartaEnZonaMagica(cartaTest);
+		assertEquals(1, campoTest.obtenerCantidadDeCartasEnZonaMagica());
+	}
 }
