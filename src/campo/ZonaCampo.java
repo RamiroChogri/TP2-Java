ba97504959c15849371bdf3f4377068f17c602fc
marcelo <marcelo@marcelo-PC-00>
2018-06-12 21:59:23 -0300

    marcelo was here

--- src/campo/ZonaCampo.java
+++ src/campo/ZonaCampo.java
@@ -1,5 +1,18 @@
 package campo;
+import carta.CartaCampo;
 
 public class ZonaCampo {
+
+	private CartaCampo cartaCampo;
+	private Boolean hayCartaCampo;
+	
+	public ZonaCampo() {
+		this.cartaCampo = new CartaCampo();
+		this.hayCartaCampo = true;
+	}
+	
+	public int obtenerCantidadDeCartas() {
+		return 0;
+	}
 
 }
