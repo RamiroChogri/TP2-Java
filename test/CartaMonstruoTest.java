5b919a518bd887eea5f6e81b5ad2e57e3739e2de
nicolas <nicolas@Nicolas-PC>
2018-06-13 16:47:38 -0300

    Agregadas pruebas de ataques entre monstruos

--- test/CartaMonstruoTest.java
+++ test/CartaMonstruoTest.java
@@ -7,22 +7,117 @@ public class CartaMonstruoTest {
 	
 	@Test
 	public void colocarCartaMonstruoEnModoAtaqueEstaEnModoAtaque() {
-		CartaMonstruo cartaMonstruo = new CartaMonstruo();
-		ZonaMonstruo zona = new ZonaMonstruo();
-		zona.colocarCarta(cartaMonstruo);
-		
+		CartaMonstruo cartaMonstruo = new CartaMonstruo(9000, 5);
 		cartaMonstruo.colocarEnModoAtaque();
 		assertEquals(true, cartaMonstruo.estaColocadaEnModoAtaque());
 	}
 	
 	@Test
 	public void colocarCartaMonstruoBocaAbajoEnModoDefensaNoEstaEnModoAtaque() {
-		CartaMonstruo cartaMonstruo = new CartaMonstruo();
+		CartaMonstruo cartaMonstruo = new CartaMonstruo(9000, 5);
 		cartaMonstruo.colocarBocaAbajoEnModoDefensa();
 		assertFalse(cartaMonstruo.estaColocadaEnModoAtaque());
 	}
 	
-
+	@Test
+	public void monstruoAtacaAOtroEnModoAtaqueConMenosAtaqueYLoDestruye() {
+		CartaMonstruo cartaMonstruo1 = new CartaMonstruo(1000, 800);
+		CartaMonstruo cartaMonstruo2 = new CartaMonstruo(500, 400);
+		cartaMonstruo1.colocarEnModoAtaque();
+		cartaMonstruo2.colocarEnModoAtaque();
+		cartaMonstruo1.atacar(cartaMonstruo2);
+		
+		assertTrue(cartaMonstruo2.estaDestruida());
+	}
 	
+	@Test
+	public void monstruoAtacaAOtroEnModoAtaqueConMenosAtaqueYNoSeDestruyeLaCartaQueAtaca() {
+		CartaMonstruo cartaMonstruo1 = new CartaMonstruo(1000, 800);
+		CartaMonstruo cartaMonstruo2 = new CartaMonstruo(500, 400);
+		cartaMonstruo1.colocarEnModoAtaque();
+		cartaMonstruo2.colocarEnModoAtaque();
+		cartaMonstruo1.atacar(cartaMonstruo2);
+		
+		assertFalse(cartaMonstruo1.estaDestruida());
+	
+	}
+	
+	@Test
+	public void monstruoAtacaAOtroMonstruoBocaArribaEnModoDefensaConMenosDefensaYLoDestruye() {
+		CartaMonstruo cartaMonstruo1 = new CartaMonstruo(1000, 800);
+		CartaMonstruo cartaMonstruo2 = new CartaMonstruo(500, 400);
+		cartaMonstruo1.colocarEnModoAtaque();
+		cartaMonstruo2.colocarBocaArribaEnModoDefensa();
+		cartaMonstruo1.atacar(cartaMonstruo2);
+		
+		assertTrue(cartaMonstruo2.estaDestruida());		
+	}
+	
+	@Test
+	public void monstruoAtacaAOtroMonstruoBocaArribaEnModoDefensaConMasDefensaYNoLoDestruye() {
+		CartaMonstruo cartaMonstruo1 = new CartaMonstruo(1000, 800);
+		CartaMonstruo cartaMonstruo2 = new CartaMonstruo(500, 1200);
+		cartaMonstruo1.colocarEnModoAtaque();
+		cartaMonstruo2.colocarBocaArribaEnModoDefensa();
+		cartaMonstruo1.atacar(cartaMonstruo2);
+		
+		assertFalse(cartaMonstruo2.estaDestruida());				
+	}
+	
+	@Test
+	public void monstruoAtacaAOtroMonstruoBocaArribaEnModoDefensaConMasDefensaYSeDestruyeLaQueAtaco() {
+		CartaMonstruo cartaMonstruo1 = new CartaMonstruo(1000, 800);
+		CartaMonstruo cartaMonstruo2 = new CartaMonstruo(500, 1200);
+		cartaMonstruo1.colocarEnModoAtaque();
+		cartaMonstruo2.colocarBocaArribaEnModoDefensa();
+		cartaMonstruo1.atacar(cartaMonstruo2);
+		
+		assertFalse(cartaMonstruo1.estaDestruida());					
+	}
+	
+	
+	@Test
+	public void monstruoAtacaAOtroMonstruoBocaAbajoEnModoDefensaConMenosDefensaYLoDestruye() {
+		CartaMonstruo cartaMonstruo1 = new CartaMonstruo(1000, 800);
+		CartaMonstruo cartaMonstruo2 = new CartaMonstruo(500, 400);
+		cartaMonstruo1.colocarEnModoAtaque();
+		cartaMonstruo2.colocarBocaAbajoEnModoDefensa();
+		cartaMonstruo1.atacar(cartaMonstruo2);
+		
+		assertTrue(cartaMonstruo2.estaDestruida());		
+	}
+	
+	@Test
+	public void monstruoAtacaAOtroMonstruoBocaAbajoEnModoDefensaConMasDefensaYNoLoDestruye() {
+		CartaMonstruo cartaMonstruo1 = new CartaMonstruo(1000, 800);
+		CartaMonstruo cartaMonstruo2 = new CartaMonstruo(500, 1200);
+		cartaMonstruo1.colocarEnModoAtaque();
+		cartaMonstruo2.colocarBocaAbajoEnModoDefensa();
+		cartaMonstruo1.atacar(cartaMonstruo2);
+		
+		assertFalse(cartaMonstruo2.estaDestruida());				
+	}
+	
+	@Test
+	public void monstruoAtacaAOtroMonstruoBocaAbajoEnModoDefensaConMasDefensaYSeDestruyeLaQueAtaco() {
+		CartaMonstruo cartaMonstruo1 = new CartaMonstruo(1000, 800);
+		CartaMonstruo cartaMonstruo2 = new CartaMonstruo(500, 1200);
+		cartaMonstruo1.colocarEnModoAtaque();
+		cartaMonstruo2.colocarBocaAbajoEnModoDefensa();
+		cartaMonstruo1.atacar(cartaMonstruo2);
+		
+		assertFalse(cartaMonstruo1.estaDestruida());					
+	}
+	
+	@Test
+	public void monstruoAtacaAOtroMonstruoBocaAbajoEnModoDefensaConMasDefensaYLoDaVuelta() {
+		CartaMonstruo cartaMonstruo1 = new CartaMonstruo(1000, 800);
+		CartaMonstruo cartaMonstruo2 = new CartaMonstruo(500, 1200);
+		cartaMonstruo1.colocarEnModoAtaque();
+		cartaMonstruo2.colocarBocaAbajoEnModoDefensa();
+		cartaMonstruo1.atacar(cartaMonstruo2);
+		
+		assertTrue(cartaMonstruo2.estaColocadaBocaArribaEnModoDefensa());					
+	}
 	
 }
