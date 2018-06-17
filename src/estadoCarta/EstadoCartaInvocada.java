131f8d20276dabfa7f0fdeb5f72d80cb00a85bd7
nicolas <nicolas@Nicolas-PC>
2018-06-17 19:46:49 -0300

    Cambios en EstadoCartaInvocada

--- src/estadoCarta/EstadoCartaInvocada.java
+++ src/estadoCarta/EstadoCartaInvocada.java
@@ -2,16 +2,30 @@ package estadoCarta;
 
 import estrategias.*;
 import modos.*;
+import cartas.*;
 
 public class EstadoCartaInvocada extends EstadoCarta {
 
 	private Modo modo;
 	private Estrategia boca;
+	private Puntos puntosDeAtaque;
+	private Puntos puntosDeDefensa;
 	
-	public EstadoCartaInvocada(Estrategia bocaRecibida, Modo modoRecibido) {
+	public EstadoCartaInvocada(Estrategia bocaRecibida, Modo modoRecibido, Puntos puntosATKRecibidos, Puntos puntosDEFRecibidos) {
 		this.modo = modoRecibido;
 		this.boca = bocaRecibida;
+		this.puntosDeAtaque = puntosATKRecibidos;
+		this.puntosDeDefensa = puntosDEFRecibidos;
 	}
+	
+	public int recibirAtaque(Puntos puntosAtaqueEnemigo) {
+	
+		int diferenciaDanio = puntosAtaqueEnemigo.obtenerDiferenciaCon(this.puntosDeAtaque);
+		return diferenciaDanio;
+	
+	}
+
+
 	
 	
 	
