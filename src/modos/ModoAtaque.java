337f7dbb4ff79f2b7f6a5a734bfe3e6dbf44968b
nicolas <nicolas@Nicolas-PC>
2018-06-17 19:22:49 -0300

    Pusheado asi lo modificas al modo jajaja

--- src/modos/ModoAtaque.java
+++ src/modos/ModoAtaque.java
@@ -8,13 +8,8 @@ public class ModoAtaque extends Modo {
 	public ModoAtaque colocarEnModoAtaque() {
 		return this;
 	}
-	
-	@Override
-	public void asignarPuntosAtaque(Puntos puntosAtaque);
-	
-	@Override
-	public void asignarPuntosDefensa(Puntos puntosDefensa);
 
+	
 	//Agregar algo que "Pemita atacar"
 	//Esto deberia ser un atributo exclusivo de las cartas Monstruo y actuar solo cuando estan
 	//invocadas
