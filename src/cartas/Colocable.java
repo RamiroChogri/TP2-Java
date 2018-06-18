5eefbc1657a77ac96ccd14a1b6194c11cc4874d1
Benjamin Ortiz <benjaortiz969@gmail.com>
2018-06-18 02:24:44 -0300

    arreglado de nuevo....

--- src/cartas/Colocable.java
+++ src/cartas/Colocable.java
@@ -4,6 +4,6 @@ import campo.*;
 
 public interface Colocable {
 	
-	public void colocarse(ZonaMonstruo monstruos ,Utilidades magicasYTrampas, EspacioCampo campo);
+	public void colocarse(ZonaMonstruo monstruos ,ZonaMagicasYTrampas magicasYTrampas, ZonaCampo campo);
 	
 }
