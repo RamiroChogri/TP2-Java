11c8b3f52c000978852a136a6a2031e7c8418c70
marcelo <marcelo@marcelo-PC-00>
2018-06-26 02:32:09 -0300

    testeando cn benja

--- src/view/handlers/BotonPantallaCompletaHandler.java
+++ src/view/handlers/BotonPantallaCompletaHandler.java
@@ -13,13 +13,20 @@ public class BotonPantallaCompletaHandler implements EventHandler<ActionEvent> {
     public BotonPantallaCompletaHandler(Stage stage, MenuItem botonPantallaCompleta) {
         this.stage = stage;
         this.botonPantallaCompleta = botonPantallaCompleta;
+        this.botonPantallaCompleta.setText("Quitar Pantalla Completa");
     }
 
     @Override
     public void handle(ActionEvent actionEvent) {
         if (!stage.isFullScreen()) {
             stage.setFullScreen(true);
+            this.botonPantallaCompleta.setText("Quitar Pantalla Completa");
             stage.show();
-        }
+        }else { 
+        	this.botonPantallaCompleta.setText("Pantalla Completa");
+        	stage.setFullScreen(false);
+        	stage.show();
+        	
+         }
     }
 }
