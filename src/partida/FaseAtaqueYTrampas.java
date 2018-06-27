bf3165e4a31049d2131d9040d9370d03e5489769
nicolas.bertillod <nicolas.bertillod@iquall.net>
2018-06-27 01:04:33 -0300

    Cerrando fase ataque y trampas

--- src/partida/FaseAtaqueYTrampas.java
+++ src/partida/FaseAtaqueYTrampas.java
@@ -8,12 +8,39 @@ import jugador.Jugador;
 
 public class FaseAtaqueYTrampas extends Fase {
 	
+	private Scanner teclado;
+
+	public FaseAtaqueYTrampas() {
+		Scanner teclado = new Scanner(System.in);
+	}
 	
 	@Override
 	public void ejecutarFase(Jugador jugadorEnTurno) {
+		String respuesta;
 		
-		Jugador jugadorEnemigo = jugadorEnTurno.obtenerJugadorEnemigo(); 
-		Scanner teclado = new Scanner(System.in);
+		do {
+			System.out.print("Desea atacar? (si/no)");
+			respuesta = teclado.nextLine();
+			
+			Jugador jugadorEnemigo = jugadorEnTurno.obtenerJugadorEnemigo(); 
+		
+			String nombreAtacante = this.pedirNombreCartaAtacante(jugadorEnTurno); 
+			String nombreAtacado = this.pedirNombreCartaAdversario(jugadorEnemigo);
+		
+			Atacable atacante, atacado;
+		
+			atacante = jugadorEnTurno.obtenerCartaDeZonaMonstruo( nombreAtacante ); 
+		
+			atacado = jugadorEnemigo.obtenerCartaDeZonaMonstruo( nombreAtacado ); 
+		
+			jugadorEnTurno.atacar( atacante , atacado );
+			
+		} while ( respuesta == "si" );
+		
+	}
+	
+	
+	public String pedirNombreCartaAtacante( Jugador jugadorEnTurno ) {
 		
 		System.out.println("Estas cartas de Monstruos estan en tu Zona de Monstruos, ingrese el nombre de la carta"); 
 		System.out.println("que quiera utilizar para atacar");
@@ -23,18 +50,23 @@ public class FaseAtaqueYTrampas extends Fase {
 			System.out.print(listaDeCartasAtacables.get(i));
 		}
 		
-		String nombreCartaMonstruoAtacante = teclado.nextLine();
+		String nombreCartaMonstruoAtacante = this.teclado.nextLine();
 		while (!listaDeCartasAtacables.contains(nombreCartaMonstruoAtacante)) {
 			System.out.println("Ingrese el nombre de una carta valida");
-			nombreCartaMonstruoAtacante = teclado.nextLine();
+			nombreCartaMonstruoAtacante = this.teclado.nextLine();
 		}
+		
+		return 	nombreCartaMonstruoAtacante;
+	}
+
+	public String pedirNombreCartaAdversario( Jugador jugadorEnemigo ) {
 		
 		System.out.println("Estas cartas de Monstruos estan en la Zona de Monstruos de tu adversario, ingrese el nombre de la carta"); 
 		System.out.println("a la que quiera atacar");
 		LinkedList<String> listaDeCartasAdversario= jugadorEnemigo.obtenerNombresDeCartasAtacablesEnZonaMonstruos();
 	
-		for (int i=0; i<listaDeCartasAtacables.size(); i++) {
-			System.out.print(listaDeCartasAtacables.get(i));
+		for (int i=0; i<listaDeCartasAdversario.size(); i++) {
+			System.out.print(listaDeCartasAdversario.get(i));
 		}
 		
 		String nombreCartaMonstruoAdversario = teclado.nextLine();
@@ -43,15 +75,7 @@ public class FaseAtaqueYTrampas extends Fase {
 			nombreCartaMonstruoAdversario = teclado.nextLine();
 		}
 		
-		
-		Atacable atacante, atacado;
-		
-		atacante = jugadorEnTurno.obtenerCartaDeZonaMonstruo( nombreCartaMonstruoAtacante ); 
-		
-		atacado = jugadorEnemigo.obtenerCartaDeZonaMonstruo( nombreCartaMonstruoAdversario ); 
-		
-		jugadorEnTurno.atacar( atacante , atacado );
-		
-		// Repetir esto hasta que el jugador decida dejar de atacar...
+		return nombreCartaMonstruoAdversario;
 	}
+	
 }
