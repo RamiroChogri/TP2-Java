bd70a8174874e4446cb796edf5205c539de939b2
Nicolas Fandos <nicolas@Nicolas-PC>
2018-06-27 03:41:40 -0300

    Modificada Fase ataque y trampas (falta modularizar)

--- src/partida/FaseAtaqueYTrampas.java
+++ src/partida/FaseAtaqueYTrampas.java
@@ -4,6 +4,8 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 import cartas.Atacable;
+import exceptions.FaltaJugadorEnemigoException;
+import exceptions.NoSePuedeAtacarAlJugadorDirectamenteException;
 import jugador.Jugador;
 
 public class FaseAtaqueYTrampas extends Fase {
@@ -18,48 +20,83 @@ public class FaseAtaqueYTrampas extends Fase {
 	
 	@Override
 	public EstadoPartida ejecutarFase(Jugador jugadorEnTurno, EstadoPartida estadoPartidaActual) {
-		String respuesta;
+		String respuesta = null;
 		EstadoPartida estadoPartidaADevolver = estadoPartidaActual;
+		LinkedList<String> listaDeCartasAtacables = jugadorEnTurno.obtenerNombresDeCartasAtacablesEnZonaMonstruos();
 		
-		System.out.print("Desea atacar? (si/no)");
-		respuesta = teclado.nextLine();
+		while (!(listaDeCartasAtacables.isEmpty()) && respuesta!="no") {
 		
-		//FALTA VER QUE SE PUEDA ATACAR DIRECTAMENTE AL JUGADOR
-		//FALTA VER QUE SI NO TE QUEDAN CARTAS PARA ATACAR QUE NO TE PREGUNTE
-		
-		while ( respuesta == "si" ) {
+			System.out.print("Desea atacar? (si/no)");
+			respuesta = this.pedirRespuestaValida();
 			
-			Jugador jugadorEnemigo = jugadorEnTurno.obtenerJugadorEnemigo(); 
-		
-			String nombreAtacante = this.pedirNombreCartaAtacante(jugadorEnTurno); 
-			String nombreAtacado = this.pedirNombreCartaAdversario(jugadorEnemigo);
-		
-			Atacable atacante, atacado;
-		
-			atacante = jugadorEnTurno.obtenerCartaDeZonaMonstruo( nombreAtacante ); 
-		
-			atacado = jugadorEnemigo.obtenerCartaDeZonaMonstruo( nombreAtacado ); 
-		
-			jugadorEnTurno.atacar( atacante , atacado );
 			
-			System.out.print("Desea seguir atacando? (si/no)");
-			respuesta = teclado.nextLine();
-		}	
+			while ( respuesta == "si" ) {
+				
+				Jugador jugadorEnemigo = jugadorEnTurno.obtenerJugadorEnemigo(); 
+			
+				String nombreAtacante = this.pedirNombreCartaAtacante(jugadorEnTurno, listaDeCartasAtacables); 
+				String nombreAtacado = this.pedirNombreCartaAdversario(jugadorEnemigo);
+			
+				Atacable atacante;
+				atacante = jugadorEnTurno.obtenerCartaDeZonaMonstruo( nombreAtacante ); 
+				
+				if (nombreAtacado == "jugador") {
+				
+					try {
+					
+						jugadorEnTurno.atacar( atacante , jugadorEnTurno.obtenerJugadorEnemigo());
+						listaDeCartasAtacables.remove(nombreAtacante);
+						System.out.print("Desea seguir atacando? (si/no)");
+						respuesta = this.pedirRespuestaValida();
+					
+					} catch (NoSePuedeAtacarAlJugadorDirectamenteException e) {
+						System.out.println("No se puede atacar al jugador");
+						respuesta = "si";
+					}
+					
+				} else {
+
+					Atacable atacado;
+					atacado = jugadorEnemigo.obtenerCartaDeZonaMonstruo( nombreAtacado ); 
+					jugadorEnTurno.atacar( atacante , atacado );
+					listaDeCartasAtacables.remove(nombreAtacante);
+					
+					System.out.print("Desea seguir atacando? (si/no)");
+					respuesta = this.pedirRespuestaValida();
+					
+				}
+				
+
+			}	
+			
+			if ((jugadorEnTurno.estaDerrotado()) || (jugadorEnTurno.obtenerJugadorEnemigo()).estaDerrotado()) {
+				estadoPartidaADevolver = new EstadoPartidaTerminada();
+			}
 		
-		if ((jugadorEnTurno.estaDerrotado()) || (jugadorEnTurno.obtenerJugadorEnemigo()).estaDerrotado()) {
-			estadoPartidaADevolver = new EstadoPartidaTerminada();
+			
 		}
-			
+				
 		return estadoPartidaADevolver;
 			
 	}
 	
 	
-	public String pedirNombreCartaAtacante( Jugador jugadorEnTurno ) {
+	public String pedirRespuestaValida() {
+		
+		String respuesta = teclado.nextLine();
+		while ((respuesta != "no") && (respuesta != "si")) {
+			System.out.println("Ingrese una respuesta valida (si/no):");
+			respuesta = teclado.nextLine();
+		}
+		
+		return respuesta;
+		
+	}
+	
+	public String pedirNombreCartaAtacante( Jugador jugadorEnTurno, LinkedList<String> listaDeCartasAtacables ) {
 		
 		System.out.println("Estas cartas de Monstruos estan en tu Zona de Monstruos, ingrese el nombre de la carta"); 
 		System.out.println("que quiera utilizar para atacar");
-		LinkedList<String> listaDeCartasAtacables = jugadorEnTurno.obtenerNombresDeCartasAtacablesEnZonaMonstruos();
 	
 		for (int i=0; i<listaDeCartasAtacables.size(); i++) {
 			System.out.print(listaDeCartasAtacables.get(i));
@@ -76,20 +113,27 @@ public class FaseAtaqueYTrampas extends Fase {
 
 	public String pedirNombreCartaAdversario( Jugador jugadorEnemigo ) {
 		
-		System.out.println("Estas cartas de Monstruos estan en la Zona de Monstruos de tu adversario, ingrese el nombre de la carta"); 
-		System.out.println("a la que quiera atacar");
+		String nombreCartaMonstruoAdversario = "jugador";
 		LinkedList<String> listaDeCartasAdversario= jugadorEnemigo.obtenerNombresDeCartasAtacablesEnZonaMonstruos();
+		
+		if (listaDeCartasAdversario.size() == 0) {
 	
-		for (int i=0; i<listaDeCartasAdversario.size(); i++) {
-			System.out.print(listaDeCartasAdversario.get(i));
-		}
-		
-		String nombreCartaMonstruoAdversario = teclado.nextLine();
-		while (!listaDeCartasAdversario.contains(nombreCartaMonstruoAdversario)) {
-			System.out.println("Ingrese el nombre de una carta valida");
+			System.out.println("Estas cartas de Monstruos estan en la Zona de Monstruos de tu adversario, ingrese el nombre de la carta"); 
+			System.out.println("a la que quiera atacar o ingrese 'jugador' para atacar directamente al adversario");
+			
+			for (int i=0; i<listaDeCartasAdversario.size(); i++) {
+				System.out.print(listaDeCartasAdversario.get(i));
+			}
+			
 			nombreCartaMonstruoAdversario = teclado.nextLine();
+			while (!listaDeCartasAdversario.contains(nombreCartaMonstruoAdversario) && (nombreCartaMonstruoAdversario != "jugador")) {
+				System.out.println("Ingrese el nombre de una carta valida");
+				nombreCartaMonstruoAdversario = teclado.nextLine();
+			}
+			
+			
 		}
-		
+	
 		return nombreCartaMonstruoAdversario;
 	}
 
