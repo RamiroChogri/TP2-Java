aac8549bb373d908f57701197d0c8d093312f6a6
nicolas.bertillod <nicolas.bertillod@iquall.net>
2018-06-27 21:52:35 -0300

    Ahora la musica de batalla comienza sola junto a la hora de de de de del duelo

--- src/view/handlers/BotonComenzarElDueloHandler.java
+++ src/view/handlers/BotonComenzarElDueloHandler.java
@@ -13,18 +13,24 @@ public class BotonComenzarElDueloHandler  implements EventHandler<ActionEvent> ,
 		Stage stage;
 	    Scene nuevaEscena;
 	    Media musicaComienzoDelDuelo;
+	    Media musicaDeBatalla;
 	    MediaPlayer reproductor;
+	    String pathMusicaBatalla = pathDeMusica+ "Hollow%20Knight%20OST%20-%20False%20Knight.wav";
 	    String pathAHoraDelDuelo = pathDeMusica+ "ES%20HORA%20DE%20DE%20DE%20DE%20DE%20DEL%20DUELO!.wav";
 	    MediaPlayer reproductorDeMusicaDeFondo;
+	    MediaPlayer reproductorBatalla;
 	    
 	    public BotonComenzarElDueloHandler(Stage stage, Scene escenaSiguiente,MediaPlayer reproductorDeMusicaDeFondo) {
 	        this.stage = stage;
 	        this.nuevaEscena = escenaSiguiente;
+	        this.musicaDeBatalla = new Media(pathMusicaBatalla);
 	        this.musicaComienzoDelDuelo = new Media(pathAHoraDelDuelo);
 	        this.reproductor = new MediaPlayer(this.musicaComienzoDelDuelo);
-	        this.reproductor.setVolume(0.5);
-	        this.reproductorDeMusicaDeFondo = reproductorDeMusicaDeFondo; 
-	        
+	        this.reproductor.setVolume(1);
+	        this.reproductorBatalla = new MediaPlayer(this.musicaDeBatalla);
+	        this.reproductorBatalla.setVolume(0.1);
+	        this.reproductorDeMusicaDeFondo = reproductorDeMusicaDeFondo;
+
 	    }
 
 	    public void handle(ActionEvent actionEvent) {
@@ -33,6 +39,7 @@ public class BotonComenzarElDueloHandler  implements EventHandler<ActionEvent> ,
 	        stage.setFullScreen(true);
 	        this.reproductorDeMusicaDeFondo.stop();
 	        this.reproductor.play();
+	        this.reproductorBatalla.play();
 	    }
 	}
 
