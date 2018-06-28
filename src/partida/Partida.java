b9928c9ae0350640d61513ca21450f8613b76381
marcelo <marcelo@marcelo-PC-00>
2018-06-28 01:31:16 -0300

    Deshecho experimento con partida

--- src/partida/Partida.java
+++ src/partida/Partida.java
@@ -8,7 +8,7 @@ import view.ContenedorDeBienvenida;
 import view.ContenedorDelDuelo;
 import view.handlers.EscOnKeyPressedHandler;
 
-public class Partida extends Application {
+public class Partida {
 
 	private Fase faseInicial;
 	private Fase fasePreparacion;
@@ -36,7 +36,6 @@ public class Partida extends Application {
 	public static void main(String[] args) {
 		Partida partida = new Partida();
 		partida.comienzaElDuelo();
-		launch(args);
 	}
 	
 	public void comienzaElDuelo() {
@@ -85,24 +84,6 @@ public class Partida extends Application {
 		
 		
 	}
-	
-	public void start(Stage stage) throws Exception {
-		 stage.setTitle("Al-go-oh! Prototype");
-		 
-		 ContenedorDelDuelo contenedorDelDuelo = new ContenedorDelDuelo(stage);
-		 Scene escenaDelDuelo = new Scene(contenedorDelDuelo,1190,670);
-		 ContenedorDeBienvenida contenedorBienvenida = new ContenedorDeBienvenida(stage,escenaDelDuelo);
-		 
-		 EscOnKeyPressedHandler salirPantallaCompleta = new EscOnKeyPressedHandler(stage,contenedorDelDuelo.getBarraDeMenu());
-		 escenaDelDuelo.setOnKeyPressed(salirPantallaCompleta);
-		
-		 Scene escenaDeBienvenida = new Scene(contenedorBienvenida,1190,670);
-		 stage.setScene(escenaDeBienvenida);
-	     stage.setFullScreenExitHint("Presione la tecla ESC para salir del modo pantalla completa");
-	     stage.setFullScreen(true);
-	     escenaDeBienvenida.getStylesheets().add("view/Style.css");
-		 stage.show();
-	 }
 	
 	public Jugador elegirQuienComienza(Jugador unJugador, Jugador otroJugador) {
 		
