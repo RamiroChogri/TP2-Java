3cc15b3b42ab8900c60371e8efc54e84559c6290
marcelo <marcelo@marcelo-PC-00>
2018-06-29 19:49:16 -0300

    Creado boton modo defensa boca arriba handler

---
+++ src/view/handlers/BotonModoDefensaBocaArribaHandler.java
@@ -0,0 +1,54 @@
+package view.handlers;
+
+import cartas.Colocable;
+import estadoCarta.EstadoCarta;
+import estadoCarta.EstadoCartaColocadaBocaArriba;
+import exceptions.NoHayLugarVacioException;
+import exceptions.NoHaySuficientesMonstruosParaSacrificarException;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import jugador.Jugador;
+import modos.Modo;
+import modos.ModoAtaque;
+import modos.ModoDefensa;
+import partida.Partida;
+import view.CajaCampo;
+
+public class BotonModoDefensaBocaArribaHandler implements EventHandler<ActionEvent>{
+
+	private Colocable carta;
+	private Partida duelo;
+	private Jugador jugador;
+	private CajaCampo cajaCampo;
+	
+	public BotonModoDefensaBocaArribaHandler(Colocable carta, Partida duelo, Jugador jugador, CajaCampo cajaCampo) {
+		this.carta = carta;
+		this.duelo = duelo;
+		this.jugador = jugador;
+		this.cajaCampo = cajaCampo;
+		
+	}
+	
+	@Override
+    public void handle(ActionEvent event) {
+        Modo modoACambiar = new ModoDefensa();
+        EstadoCarta estadoACambiar = new EstadoCartaColocadaBocaArriba();
+        try {
+        	try {
+        		carta.cambiarA(modoACambiar);
+        		jugador.colocar(carta, estadoACambiar);
+        		jugador.eliminarCartaDeLaMano(carta.obtenerNombre());
+        		duelo.setSeJugoCartaMonstruo();
+        		if (duelo.estaYugiEnTurno()) {
+        			cajaCampo.actualizarVistaYugiEnTurno(jugador, jugador.obtenerJugadorEnemigo());
+        		} else {
+        			cajaCampo.actualizarVistaKaibaEnTurno(jugador, jugador.obtenerJugadorEnemigo());
+        		}
+        	} catch (NoHaySuficientesMonstruosParaSacrificarException e) {
+        	
+        	} 
+        }catch (NoHayLugarVacioException e) {
+    	
+        }
+    }
+}
