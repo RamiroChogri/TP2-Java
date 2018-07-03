9d7312e0d8146c5a2cd655dc47a8d5dc716e503a
marcelo <marcelo@marcelo-PC-00>
2018-07-02 21:31:58 -0300

    arreglado cementerio

--- src/view/EspacioCementerio.java
+++ src/view/EspacioCementerio.java
@@ -16,8 +16,9 @@ public class EspacioCementerio extends StackPane{
 			Rectangle espacioCementerio = new Rectangle(60,100,Color.GREY);
 			espacioCementerio.setStroke(Color.WHITE);
 			
-			Text textoZona = new Text(" Cartas\nMuertas");
+			Text textoZona = new Text(" Cementerio");
 			textoZona.setStroke(Color.WHITE);
+			textoZona.setStyle("-fx-font-size: 10px;");
 			
 			this.getChildren().addAll(espacioCementerio, textoZona);
 			this.setAlignment(Pos.CENTER);
