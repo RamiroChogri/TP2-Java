691ee92df43f8e5792e0b239cf53905c24c2b170
Nicolas Fandos <nicolas@Nicolas-PC>
2018-07-03 20:20:02 -0300

    Actualizado boton de como jugar

--- src/view/handlers/BotonComoJugarHandler.java
+++ src/view/handlers/BotonComoJugarHandler.java
@@ -12,8 +12,14 @@ public class BotonComoJugarHandler implements EventHandler<ActionEvent> , PathAr
 	 public void handle(ActionEvent actionEvent) {
 	        Alert alert = new Alert(AlertType.INFORMATION);
 	        alert.setTitle("Como Jugar");
-	        alert.setHeaderText("Este link contiene un archivo que contiene las reglas de juego.");
-	        String mensaje = "https://github.com/RamiroChogri/TP2-Java/blob/master/README.md";
+	        alert.setHeaderText("Pulse el boton seccundario del raton para interactuar con las cartas en la fase que corresponda.\n"
+	        		+ "-En la fase de preparacion se pueden colocar cartas de la mano y rotar los monstruos colocados.\n"
+	        		+ "(Aclaracion: Solo se puede jugar una carta monstruo por turno y no se puede rotar en el turno en que se coloca)\n"
+	        		+ "-En la fase de ataques y trampas se puede atacar al enemigo (Y se activaran las trampas en caso de que corresponda).\n"
+	        		+ "(Aclaracion: No se puede atacar en el primer turno y los monstruos solo pueden atacar una vez por turno)\n"
+	        		+ "-En la fase final se pueden voltear y activar las cartas magicas boca abajo.\n"
+	        		+ "Gana el primer jugador que logre que su contrincate pierda sus 8000 puntos de vida.");
+	        String mensaje = "El jugador que tenga a la vez todas las partes de exodia en la mano gana automaticamente.";
 	        alert.setContentText(mensaje);
 	        alert.show();
 	    }
