8e0af45ff9fbce5e50bfcd9b59f3f743532d9ad2
marcelo <marcelo@marcelo-PC-00>
2018-07-03 02:03:50 -0300

    fix path imagenes

--- src/viewSupportFiles/PathArchivos.java
+++ src/viewSupportFiles/PathArchivos.java
@@ -4,7 +4,7 @@ public interface PathArchivos {
 	//Ambos tienen que tener el "file:///" antes, el de musica tiene que ser con // entre directorios,
 	//el de imagenes con / esta bien, no puede haber espacios, en donde iria un espacio se pone un %20.
 	//CAMBIAR EL PATH EN ARCHIVO .CSS SECCION ROOT 
-	String pathDeImagenes = "file:///C:/Users/marcelo/Documents/Desktop/Algo%203/TP2-Java/src/viewSupportFiles/";
+	String pathDeImagenes = "file:src/viewSupportFiles/";
 	String pathDeMusica = "file:///C://Users//marcelo//Documents//Desktop//Algo%203//TP2-Java//src//viewSupportFiles/";
-	String pathDePackCartas ="file:///C:/Users/marcelo/Documents/Desktop/Algo%203/TP2-Java/src/viewSupportFiles/imagenesCartas/";
+	String pathDePackCartas ="file:src/viewSupportFiles/imagenesCartas/";
 }
